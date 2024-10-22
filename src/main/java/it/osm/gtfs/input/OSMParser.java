46eaa69e2cd8099bda8e56a7bcbe7afd4dd4c78c
Emil Linus Albrecht <emil.albrecht27@gmail.com>
2024-10-22 03:09:59 +0200

    fix ignoring unknown stop types

--- src/main/java/it/osm/gtfs/input/OSMParser.java
+++ src/main/java/it/osm/gtfs/input/OSMParser.java
@@ -204,8 +204,11 @@ public class OSMParser {
                 continue;
             }
 
-            if (osmStop.getStopType() == null) //we don't know the type of this stop based on the tag values we checked
-                throw new IllegalArgumentException("Unknown node type for OSM node ID: " + osmStop.getOSMId() + ". We support only highway=bus_stop, public_transport=stop_position, railway=tram_stop, railway=station and station=subway");
+            if (osmStop.getStopType() == null) {
+                //we don't know the type of this stop based on the tag values we checked
+                System.out.println(ansi().render("@|yellow Unknown node type for OSM node ID: " + osmStop.getOSMId() + ". We support only highway=bus_stop, public_transport=stop_position, railway=tram_stop, railway=station and station=subway"));
+                continue;
+            } 
 
             if (osmStop.getStopType().equals(OSMStopType.GENERAL_STOP_POSITION)) {
                 System.out.println(ansi().render("@|yellow Ignoring general_stop_position... (node ID: " + osmStop.getOSMId() + ") |@"));
